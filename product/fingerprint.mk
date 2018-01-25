0598dca2e6709992fcbfa532670707884ec317e6
DarkLord1731 <darklord1731@gmail.com>
2018-01-25 15:47:32 +0530

    on7xelte: Build new Fingerprint Hal for O
    This replaces fingerprintd

--- product/fingerprint.mk
+++ product/fingerprint.mk
@@ -19,4 +19,5 @@ PRODUCT_PACKAGES += \
     fingerprintd \
     mcDriverDaemon \
     fingerprint.exynos5 \
-    libbauthtzcommon_shim
+    libbauthtzcommon_shim \
+    android.hardware.biometrics.fingerprint@2.1-service
