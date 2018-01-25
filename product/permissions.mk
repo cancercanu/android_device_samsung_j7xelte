696b05d9c2fefb631e0ed30590650b8fcea11ea0
DarkLord1731 <darklord1731@gmail.com>
2018-01-25 15:51:29 +0530

    on7xelte: Update proprietary blob list

--- product/permissions.mk
+++ product/permissions.mk
@@ -18,4 +18,4 @@
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
-    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml
